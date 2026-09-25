3c00ff6d30f8b8cbc2ba24eaab0b5c3c2e696ae1
Greenhill Efka <16738560+GreenhillEfka@users.noreply.github.com>
2026-09-25 14:17:18 +0200

    feat: expose exact helper reconciliation in zone context

--- pilotsuite/pilotsuite/app.py
+++ pilotsuite/pilotsuite/app.py
@@ -370,6 +370,8 @@ async def _context_payload(service, zone_id, export=False):
     report['foundation']['provisioning'] = provisioning_preview(inventory, report['foundation'])
     from pilotsuite.core.presence_foundation import presence_contract
     report['foundation']['presence_contract'] = presence_contract(report['foundation'])
+    from pilotsuite.core.helper_reconciliation import reconcile_helpers
+    report['foundation']['helper_reconciliation'] = reconcile_helpers(report['foundation'], report['candidates'])
     from pilotsuite.core.guide import zone_guide
     zone_result = next((z for z in service._zone_results if z['zone_id'] == zone_id), {})
     report['guide'] = zone_guide(inventory, report, await service.status(), zone_result.get('summary', {}))
